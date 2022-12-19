ae9683a07771fc5e67b10de9c0934f99a08fa844
manifet <manifetmain@gmail.com>
2022-12-19 20:25:22 +0300

    mistakes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,22 @@
 const hamburger = document.querySelector(".header__hamburger")
 const menu = document.querySelector(".header__menu")
+const menuTitles = document.querySelectorAll(".header__menu-title")
 
 hamburger.addEventListener("click", () => {
     menu.classList.add("active")
 })
-
 document.addEventListener("click", (e) => {
-    const composed = e.composedPath()
-    const withinBoundaries = composed.includes(menu) || composed.includes(hamburger);
-    if (!withinBoundaries) {
-        menu.classList.remove("active")
-    }
+    const target = e.target
+    if (!target.closest('.header__menu') && !target.closest('.header__hamburger')) menu.classList.remove("active")
+})
+
+menuTitles.forEach((item) => {
+    item.addEventListener("click", () => {
+        menuTitles.forEach((item) => {
+            const menuItem = item.parentElement
+            menuItem.classList.remove("active")
+        })
+        const menuItem = item.parentElement
+        menuItem.classList.add("active")
+    })
 })
